a405fa53e689487f6bb550f81499e78b84afa685
Peter Damoc <pdamoc@gmail.com>
2015-07-05 12:22:09 +0300

    Cosmetic change to the code.

--- challenge1.elm
+++ challenge1.elm
@@ -10,12 +10,15 @@ view: (Int, Int) -> Int -> Element
 view (w, h) x = 
       let 
         (bkg_color, text_color, text) = 
-            if x < w //2 then (purple, white, "Left") 
-                else (lightBlue, black, "Right")
+            if   x < w //2
+            then (purple, white, "Left") 
+            else (lightBlue, black, "Right")
       in 
-        color bkg_color (container w h middle 
-            (centered (coloredText text text_color)))
+        color bkg_color
+        <| container w h middle 
+        <| centered
+        <| coloredText text text_color
       
 main : Signal Element
 main =
-  Signal.map2 view Window.dimensions Mouse.x 
+  Signal.map2 view Window.dimensions Mouse.x
