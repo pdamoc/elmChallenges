4a691667e1b7e8db43749f3a8fbab1001cdaebed
Peter Damoc <pdamoc@gmail.com>
2015-11-26 16:53:57 +0200

    updated to use Elm Architecture.

--- challenge4.elm
+++ challenge4.elm
@@ -1,4 +1,3 @@
-import Char
 import Html exposing (..)
 import Html.Attributes as Attr exposing (..)
 import Html.Events exposing (..)
@@ -9,34 +8,111 @@ import Task exposing (..)
 import Set
 import String exposing (join )
 import Time exposing (since, second, Time)
-import Signal exposing (sampleOn, dropRepeats)
-import Debug
+import Signal exposing (sampleOn, dropRepeats, Address)
+import StartApp 
+import Effects exposing (Effects)
+
+-- MODEL
+
+type alias User = 
+  { name : String
+  , avatar_url : String
+  , repos_url : String
+  , languages : List String
+  }
+
+type alias Model = 
+  { query : String
+  , lastKeyPress : Maybe Time
+  , user : Maybe User
+  }
+
+init : Model 
+init = { query = "evancz", lastKeyPress = Nothing, user = Nothing}
+
+-- UPDATE 
+
+type Action = Update (Maybe User) | UpdateQuery String | Tick Time
+
+lookupUser : String -> Effects Action
+lookupUser query =
+  ((Http.get decodeUser ("http://api.github.com/users/" ++ query))
+  `andThen` \user ->
+    (Http.get decodeLanguages user.repos_url `onError` 
+      (\msg -> succeed [toString msg]))
+  `andThen` \languages -> 
+      let 
+        user' : User
+        user' =  { user | languages = notEmptyUnique languages }
+      in succeed user') 
+  |> Task.toMaybe 
+  |> Task.map Update
+  |> Effects.task
+
+notEmptyUnique : List String -> List String
+notEmptyUnique xs = 
+  List.filter (\x -> not <| String.isEmpty x) <| Set.toList <| Set.fromList xs
+
+decodeLanguages : Json.Decoder (List (String))
+decodeLanguages = (Json.list  <| Json.oneOf 
+  [ (Json.at ["language"] Json.string)
+  , (Json.succeed "")
+  ])
+
+decodeUser : Json.Decoder (User)
+decodeUser = Json.object4 User
+    ("name" := Json.string) 
+    ("avatar_url" := Json.string)
+    ("repos_url" := Json.string)
+    (Json.succeed [])
+
+update : Action -> Model -> (Model, Effects Action)
+update action model = 
+  case action of
+    UpdateQuery str -> 
+      ({ model | query = str}, Effects.tick Tick)
+    
+    Tick time -> 
+      case model.lastKeyPress of
+        Nothing -> ({model| lastKeyPress = Just time}, Effects.tick Tick)
+        Just t ->  
+          if (time - t) > second 
+          then ({model| lastKeyPress = Just t}, lookupUser model.query)
+          else (model, Effects.tick Tick)
+
+    Update user -> 
+      ({ model | user = user}, Effects.none)
 
 -- VIEW
 
-view : String -> Result String (User) -> Html
-view string result =
+view : Address Action  -> Model -> Html
+view address model =
   let field =
         input
           [ placeholder "Please enter the GitHub username"
-          , value string
-          , on "input" targetValue (Signal.message query.address)
+          , value model.query
+          , on "input" targetValue (\s -> Signal.message address (UpdateQuery s))
           , myStyle
           ]
           []
 
       messages =
-        case result of
-          Err msg ->
+        case model.user of
+          Nothing ->
+            let
+              msg = case model.lastKeyPress of 
+                Nothing -> "Looking for user..."
+                Just t -> "User not found :("
+            in
               [ div [ myStyle ] [ text msg ] ]
 
-          Ok user ->
+          Just user ->
               [ div [ myStyle ] [ text user.name ]
               , img  [ src user.avatar_url, imgStyle] []
               , div [ myStyle ] [ text <| knownLanguages user.languages]
               ]
   in
-      div [] ((div [ myStyle ] [ text "GitHub Username" ]) :: field :: messages)
+    div [] ((div [ myStyle ] [ text "GitHub Username" ]) :: field :: messages)
 
 
 knownLanguages : List String -> String 
@@ -65,68 +141,18 @@ myStyle =
 
 -- WIRING
 
-main =
-  Signal.map2 view query.signal results.signal
+
+app : StartApp.App Model
+app = StartApp.start 
+  { init = (init, lookupUser init.query)
+  , update = update
+  , view = view
+  , inputs = [] }
+
+main : Signal Html
+main = app.html
+
+port tasks : Signal (Task Effects.Never ())
+port tasks = app.tasks
 
 
-query : Signal.Mailbox String
-query =
-  Signal.mailbox "evancz"
-
-
-results : Signal.Mailbox (Result String (User)) 
-results =
-  Signal.mailbox (Err "")
-
-maybeQuery : Time -> Signal a -> Signal (Maybe a)
-maybeQuery time sig =
-  Signal.map2 (\b s -> if b then Just s else Nothing) (Signal.map not (time `since` query.signal)) sig
-
-port requests : Signal (Task x ())
-port requests =
-  flip Signal.map (maybeQuery second query.signal)
-    (\q ->
-      case q of
-        Nothing ->
-          Task.succeed ()
-        
-        Just str ->
-          Task.toResult (lookupUser str) `andThen` Signal.send results.address)
-
-
-lookupUser : String -> Task String (User)
-lookupUser query =
-  succeed ("http://api.github.com/users/" ++ query)
-  `andThen` (mapError (always "User not found :(") << Http.get decodeUser)
-  `andThen` \user ->
-    (Http.get decodeLanguages user.repos_url `onError` 
-      (\msg -> succeed [toString msg]))
-  `andThen` \languages -> 
-      let 
-        user' : User
-        user' =  { user | languages = notEmptyUnique languages }
-      in succeed user' 
-
-notEmptyUnique : List String -> List String
-notEmptyUnique xs = 
-  List.filter (\x -> not <| String.isEmpty x) <| Set.toList <| Set.fromList xs
-
-decodeLanguages : Json.Decoder (List (String))
-decodeLanguages = (Json.list  <| Json.oneOf 
-  [ (Json.at ["language"] Json.string)
-  , (Json.succeed "")
-  ])
-
-type alias User = 
-  { name: String
-  , avatar_url: String
-  , repos_url: String
-  , languages: List String
-  }
-
-decodeUser : Json.Decoder (User)
-decodeUser = Json.object4 User
-    ("name" := Json.string) 
-    ("avatar_url" := Json.string)
-    ("repos_url" := Json.string)
-    (Json.succeed [])
